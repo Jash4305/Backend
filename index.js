66aff172715817fec55ec96baff646f45f77d365
jash <daltonjd45@gmail.com>
2024-05-03 00:20:35 +0530

    db connect

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 
-const express = require('express');
-const mongoose = require('mo')
+const express=require('express')
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+
+const app=express()
+dotenv.config()
+
+//to connect with database with url
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully!")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+// calling connect and passing port to establish the connection
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running here http://localhost:"+process.env.PORT)
+})
